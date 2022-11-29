6516ec3f0c1f463c763eb2bde6539a42bac4e7bc
Rafael Schmitt <rafael.claycon@gmail.com>
2022-11-28 21:11:28 -0300

    Initial commit

---
+++ QuadroMetas/QuadroMetasApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuadroMetasApp.swift
+//  QuadroMetas
+//
+//  Created by Rafael Claycon Schmitt on 28/11/22.
+//
+
+import SwiftUI
+
+@main
+struct QuadroMetasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
